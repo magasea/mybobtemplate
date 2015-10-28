77184ceb71a1fd01c034f21f410703c605888c48
magasea <magaseaocp@gmail.com>
2015-10-28 14:46:52 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mybobtemplate',
+      version='0.1',
+      description='The mybobtemplate',
+      url='http://github.com/bobtemplate/mybobtemplate',
+      author='david chen',
+      author_email='wei.chen@atmel.com',
+      license='MIT',
+      packages=['mybobtemplate'],
+      zip_safe=False)
